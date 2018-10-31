9bd481484260464ea4f1be26515fd9ee358a7db1
Stéphane Guinot <sguinot86@gmail.com>
2018-10-31 08:36:40 +0100

    login in progress

--- src/pages/Receiver.jsx
+++ src/pages/Receiver.jsx
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
 import Bonbondex from '../Components/Bonbondex.jsx';
 import MapContainer from '../Components/Map';
+import Login from '../Components/Login'
+
 
 class Receiver extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      loggedUser:false,
+    }
+  }
+
+  setLoggedUser=()=>{
+    this.setState({
+      loggedUser: true,
+    })
+  }
+
   render() {
     return (
-      <div>
-        <MapContainer 
-          giversArray={this.props.giversArray}
-        /> 
-        <Bonbondex 
-          giversArray={this.props.giversArray}
+      <div>{
+        this.state.loggedUser ?
+        <div>
+          <MapContainer 
+            giversArray={this.props.giversArray}
+          /> 
+          <Bonbondex 
+            giversArray={this.props.giversArray}
+          />
+        </div>
+        :
+        <Login 
+          userStatus = {this.props.userStatus}
+          setLoggedUser = {this.setLoggedUser}
         />
-      </div>
+      }</div>
     );
   }
 }
